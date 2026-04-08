237063d3bf6c08233d650cf7362ae25432342c5b
Anh <anhdt1911.work@gmail.com>
2026-04-08 12:34:02 +0700

    76 minimum window substring

--- src/76_minimum_window_substring/main.go
+++ src/76_minimum_window_substring/main.go
@@ -7,41 +7,38 @@ func minWindow(s string, t string) string {
 	if len(t) > len(s) {
 		return ""
 	}
-	tFreq := make(map[byte]int)
-	sFreq := make(map[byte]int)
-	result := ""
+	tMap := make(map[byte]int)
+	sMap := make(map[byte]int)
 	for i := range t {
-		tFreq[t[i]]++
-		sFreq[s[i]]++
+		tMap[t[i]]++
 	}
+	result := ""
+	uniqueChar := len(tMap)
 	i := 0
-	for j := len(t); j < len(s); j++ {
-		if !isValid(tFreq, sFreq) {
-			sFreq[s[j]]++
+	count := 0
+	for j := 0; j < len(s); j++ {
+		sMap[s[j]]++
+		if v, ok := tMap[s[j]]; ok && v == sMap[s[j]] {
+			count++
+		}
+		if count < uniqueChar {
 			continue
 		}
-		//ABC
-		//DABCD j 4  i 2 4-1 2-1
-		for isValid(tFreq, sFreq) {
-			sFreq[s[i]]--
+
+		for count == uniqueChar {
+			best := s[i : j+1]
+			if result == "" || len(best) < len(result) {
+				result = best
+			}
+			sMap[s[i]]--
+			if v, ok := tMap[s[i]]; ok && v > sMap[s[i]] {
+				count--
+			}
 			i++
-		}
-		currentBest := s[i-1 : j+1]
-		if result == "" || len(currentBest) < len(result) {
-			result = currentBest
 		}
 
 	}
 	return result
-}
-
-func isValid(t, s map[byte]int) bool {
-	for k := range t {
-		if t[k] != s[k] {
-			return false
-		}
-	}
-	return true
 }
 
 func main() {
