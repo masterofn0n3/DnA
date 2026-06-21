89203cd2b8372c41d4a48f79aa7ea1c3bd209917
Anh Duong Tien <anhdt1911.work@gmail.com>
2026-06-21 17:35:29 +0800

    213 house robber ii

---
+++ src/213_house_robber_ii/main.go
@@ -0,0 +1,27 @@
+package main
+
+func rob(nums []int) int {
+	return max(robHelper(nums[1:]), robHelper(nums[:len(nums)-1]))
+}
+
+func robHelper(nums []int) int {
+	// dp[i] = max ammount can rob
+	// i = 0 dp = 1
+	// i = 1 dp = 2, can not robb adjacent
+	// i = 3 dp = 4, max(dp[i-2]+cost[i], dp[i-1])
+	if len(nums) == 1 {
+		return nums[0]
+	}
+
+	if len(nums) == 2 {
+		return max(nums[0], nums[1])
+	}
+
+	dp := []int{nums[0], max(nums[1], nums[0])}
+
+	for i := 2; i < len(nums); i++ {
+		dp = append(dp, max(dp[i-2]+nums[i], dp[i-1]))
+	}
+
+	return dp[len(nums)-1]
+}
