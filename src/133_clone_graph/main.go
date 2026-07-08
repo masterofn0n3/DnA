c52c3a868f59bbd93c1b7b9daef80277f065ff74
Anh <anhdt1911.work@gmail.com>
2026-07-08 11:11:19 +0700

    fix

--- src/133_clone_graph/main.go
+++ src/133_clone_graph/main.go
@@ -15,24 +15,24 @@ func cloneGraph(node *Node) *Node {
 
 	for len(queue) > 0 {
 		current := queue[0]
-		if _, ok := nodeMap[current]; !ok {
-			newNei := []*Node{}
-			for _, nei := range current.Neighbors {
-				if _, ok := nodeMap[nei]; !ok {
-					nodeMap[nei] = &Node{
-						Val: nei.Val,
-					}
-					queue = append(queue, nei)
-				}
-				newNei = append(newNei, nodeMap[nei])
-			}
-			nodeMap[current] = &Node{
-				Val:       current.Val,
-				Neighbors: newNei,
-			}
-
-		}
 		queue = queue[1:]
+		if _, ok := nodeMap[current]; !ok {
+			nodeMap[current] = &Node{
+				Val: current.Val,
+			}
+		}
+		newNei := []*Node{}
+		for _, nei := range current.Neighbors {
+			if _, ok := nodeMap[nei]; !ok {
+				nodeMap[nei] = &Node{
+					Val: nei.Val,
+				}
+				queue = append(queue, nei)
+			}
+			newNei = append(newNei, nodeMap[nei])
+		}
+		nodeMap[current].Neighbors = newNei
+
 	}
 
 	return nodeMap[node]
